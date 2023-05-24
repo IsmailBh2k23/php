59c5420c1d4d2be74047418d32703b557520dfda
IsmailBh2k23 <fa3ilou@gmail.com>
2023-05-24 20:50:31 +0100

    php

---
+++ welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+    
+Id de l'article: <?php echo $_POST["id"]?><br>
+Nom de L'article: <?php echo $_POST["name"]; ?><br>
+Prix de l'article: <?php echo $_POST["price"]; ?><br>
+Quantité de l'article: <?php echo $_POST["qte"]; ?><br>
+
+</body>
+</html>
